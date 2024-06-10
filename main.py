e09cc04d0f7815665f9205395e0dd47cbb97d8a3
s-damasceno <samueldamasceno.vip@gmail.com>
2024-06-10 18:14:33 -0300

    validação adicionada

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ def regras():
     print("""Já conhece as regras?
       \t1.Sim
       \t2.Não""")
-    opcao = input("Escolha uma opção: ")
-    match opcao:
-        case "1":
+    while True:
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
             print("Ótimo! Então vamos começar.")
             return
-        case "2":
+        elif opcao == "2":
             print("""\tFunciona assim:
                   Em cada rodada, vou pedir para você escolher entre três opções: pedra, papel ou tesoura.
                   Então, o computador irá escolher aleatoriamente entre as mesmas opções.
@@ -22,46 +22,57 @@ def regras():
                   Vamos lá?""")
             input("Digite ENTER para continuar.")
             return
+        else:
+            print("Opção inválida. Por favor, escolha 1 ou 2.")
 
 def pedra_papel_tesoura():
-    print("Pedra, papel ou tesoura?")
-    print("\t1. Pedra \n\t2. Papel \n\t3. Tesoura")
-    print()
-    jogada_usurio = input("Escolha sua opção: ")
-    print()
+
+    while True:
+        print("Pedra, papel ou tesoura?")
+        print("\t1. Pedra \n\t2. Papel \n\t3. Tesoura")
+        print()
+        jogada_usuario = input("Escolha sua opção: ")
+        if jogada_usuario in ["1", "2", "3"]:
+            break
+        else:
+            print("Opção inválida.")
 
     jogada_computador = random.choice(["Pedra", "Papel", "Tesoura"])
-    match jogada_usurio:
+
+    match jogada_usuario:
         case "1":
+            jogada_usuario = "Pedra"
             print("Você: Pedra")
-            print("Computador: ", jogada_computador)
-            print()
-            if jogada_computador == "pedra":
-                print("Empate")
-            elif jogada_computador == "papel":
-                print("Você perdeu")
-            elif jogada_computador == "tesoura":
-                print("Você ganhou")
+
         case "2":
-            print("Você escolheu Papel")
-            print("Computador escolheu", jogada_computador)
-            print()
-            if jogada_computador == "pedra":
-                print("Você ganhou")
-            elif jogada_computador == "papel":
-                print("Empate")
-            elif jogada_computador == "tesoura":
-                print("Você perdeu")
+            jogada_usuario = "Papel"
+            print("Você: Papel")
+
         case "3":
-            print("Você escolheu Tesoura")
-            print("Computador escolheu", jogada_computador)
-            print()
-            if jogada_computador == "pedra":
-                print("Você perdeu")
-            elif jogada_computador == "papel":
-                print("Você ganhou")
-            elif jogada_computador == "tesoura":
-                print("Empate")
+            jogada_usuario = "Tesoura"
+            print("Você: Tesoura")
+
+    calcular_resultado(jogada_usuario, jogada_computador)
+
+def calcular_resultado(jogada_usuario, jogada_computador):
+    if jogada_usuario == jogada_computador:
+        print("Empate!")
+    elif jogada_usuario == "Pedra":
+        if jogada_computador == "Tesoura":
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
+    elif jogada_usuario == "Papel":
+        if jogada_computador == "Pedra":
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
+    elif jogada_usuario == "Tesoura":
+        if jogada_computador == "Papel":
+            print("Você ganhou!")
+        else:
+            print("Você perdeu!")
+
 
 print("Vamos jogar Pedra, Papel ou Tesoura?")
 print()
